29d2f8d89fee11dbd91ac131333b7ac00662dc5a
Linus Östberg <linus@talavis.eu>
2016-06-01 16:18:37 +0200

    Problem 5 solved by analysis

---
+++ p5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+'''2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?'''
+
+#prime factors:
+#2   2
+#3   3
+#4   2*2
+#5   5
+#6   2*3
+#7   7
+#8   2*2*2
+#9   3*3
+#10  5*2
+#11  11
+#12  2*2*3
+#13  13
+#14  2*7
+#15  3*5
+#16  2*2*2*2
+#17  17
+#18  2*3*3
+#19  19
+#20  2*2*5
+
+#the sum should be the product of the highest occurence of each factor, e.g.:
+#2*2*2*2*3*3*5*7*11*13*17*19
