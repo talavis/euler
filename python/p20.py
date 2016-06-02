e94a440adf23c555359d9b3891f6c43b5868a142
Linus Östberg <linus@talavis.eu>
2016-06-02 10:42:57 +0200

    Problem 20 solved

---
+++ python/p20.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!'''
+
+import math
+
+product = math.factorial(100)
+digsum = 0
+
+for d in str(product) :
+    digsum += int(d)
+
+print(digsum)
